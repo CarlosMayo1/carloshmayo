e2612e5c58da2c59c5e30ac7c95c030b6d350075
CarlosMayo1 <carloshmayo@gmail.com>
2020-09-21 15:15:17 -0500

    Updates

---
+++ precache-manifest.7b4eeb7596b6227c6cc1e1e5eb6ac430.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935b00fd382759dcceb0c62b876486d7",
+    "url": "/CarlosMayo1/carloshmayo.git/index.html"
+  },
+  {
+    "revision": "b83925e7594a23578380",
+    "url": "/CarlosMayo1/carloshmayo.git/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "9f1a2065d2338804dbb7",
+    "url": "/CarlosMayo1/carloshmayo.git/static/css/main.f688a91d.chunk.css"
+  },
+  {
+    "revision": "b83925e7594a23578380",
+    "url": "/CarlosMayo1/carloshmayo.git/static/js/2.d98c931f.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/CarlosMayo1/carloshmayo.git/static/js/2.d98c931f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f1a2065d2338804dbb7",
+    "url": "/CarlosMayo1/carloshmayo.git/static/js/main.da79cb79.chunk.js"
+  },
+  {
+    "revision": "69547ad357648f93cb0d",
+    "url": "/CarlosMayo1/carloshmayo.git/static/js/runtime-main.93da2ddf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8c9d8752fda849b3bd11f95b73490427.js"
+  "/CarlosMayo1/carloshmayo.git/precache-manifest.7b4eeb7596b6227c6cc1e1e5eb6ac430.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CarlosMayo1/carloshmayo.git/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
