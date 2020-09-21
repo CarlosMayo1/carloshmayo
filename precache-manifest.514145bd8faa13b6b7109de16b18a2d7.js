e691f6037f481409ca78e19aa8fe13979a77cd52
CarlosMayo1 <carloshmayo@gmail.com>
2020-09-20 19:44:06 -0500

    Updates

---
+++ precache-manifest.514145bd8faa13b6b7109de16b18a2d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e059798fd688a5d33948127664c02ef",
+    "url": "/carloshmayo/index.html"
+  },
+  {
+    "revision": "b83925e7594a23578380",
+    "url": "/carloshmayo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "12811f556720092f2d7e",
+    "url": "/carloshmayo/static/css/main.f688a91d.chunk.css"
+  },
+  {
+    "revision": "b83925e7594a23578380",
+    "url": "/carloshmayo/static/js/2.d98c931f.chunk.js"
+  },
+  {
+    "revision": "b6840d1fa4cd3c3e88b7c215b28b2e9e",
+    "url": "/carloshmayo/static/js/2.d98c931f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12811f556720092f2d7e",
+    "url": "/carloshmayo/static/js/main.7c3cae94.chunk.js"
+  },
+  {
+    "revision": "14c11ad0440e734b0a97",
+    "url": "/carloshmayo/static/js/runtime-main.eb2ba35d.js"
+  }
+]);
