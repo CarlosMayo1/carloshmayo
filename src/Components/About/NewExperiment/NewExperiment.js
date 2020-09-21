657af76c1bfb5ffb88b86bc9184d4db79ef1f369
CarlosMayo1 <carloshmayo@gmail.com>
2020-09-21 14:49:25 -0500

    Adding the NewExperiment file

---
+++ src/Components/About/NewExperiment/NewExperiment.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const newExperiment = () => {
+  return(
+    <div>
+      <h3>I am doing a succesful experiment</h3>
+      <p>This is a new experiment</p>
+    </div>
+  )
+};
+
+export default newExperiment;
