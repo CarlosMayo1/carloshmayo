305921cf56dff61842cbc4e50461d4b76b688764
CarlosMayo1 <carloshmayo@gmail.com>
2020-09-21 22:20:31 -0500

    Adding the name of the folder to the links in the Navbar file

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -9,9 +9,9 @@ const navbar = () => {
       <nav className="navbar">
         <span className="navbarBrand"><a href="/logo">Logo</a></span>
         <ul className="navbarItems">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="about">About me</Link></li>
-          <li><Link to="/contact">Contanct</Link></li>
+          <li><Link to="/carloshmayo/">Home</Link></li>
+          <li><Link to="/carloshmayo/about">About me</Link></li>
+          <li><Link to="/carloshmayo/contact">Contanct</Link></li>
         </ul>
       </nav>
     </header>
