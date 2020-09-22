20acb291648ee4e6bb31c72e46533bf5b1dbef0e
CarlosMayo1 <carloshmayo@gmail.com>
2020-09-21 22:08:29 -0500

    Deleting NewExperiment, it is not longer required

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,16 +3,12 @@ import React from 'react';
 import Teachers from './Teachers/Teachers';
 import GridStudents from '../Grid/GridStudents/GridStudents';
 
-// This is another expriment
-import NewExperiment from './NewExperiment/NewExperiment';
-
 const about = () => {
   return(
     <div className="container">
       <Teachers/>
       <section>
         <GridStudents/>
-        <NewExperiment/>
       </section>
     </div>
   )
